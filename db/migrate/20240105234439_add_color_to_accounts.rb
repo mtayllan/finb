1f166f0e110a8e937796c3e44bef8eb51ca4750e
mtayllan <messiastayllan@gmail.com>
2024-01-05 20:47:02 -0300

    Add color to accounts table

---
+++ db/migrate/20240105234439_add_color_to_accounts.rb
@@ -0,0 +1,5 @@
+class AddColorToAccounts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :accounts, :color, :string, null: false, default: '#000000'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_05_223432) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_05_234439) do
   create_table "accounts", force: :cascade do |t|
     t.string "name", null: false
     t.decimal "initial_balance", precision: 9, scale: 2, default: "0.0", null: false
     t.decimal "balance", precision: 9, scale: 2, default: "0.0", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color", default: "#000000", null: false
   end
 
   create_table "categories", force: :cascade do |t|
