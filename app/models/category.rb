3a2b6d2d90c8df9d3985baa4c4eb806613022a9e
mtayllan <messiastayllan@gmail.com>
2024-01-06 09:55:01 -0300

    Add colro as background of icon on categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,6 +6,6 @@ class Category < ApplicationRecord
   validates :name, :color, :icon, presence: true
 
   def icon_svg
-    Icons::ICONS[icon.to_sym] || Icons::ICONS[:question]
+    Icons.fetch(icon.to_sym, background: color)
   end
 end
