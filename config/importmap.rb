0cf5239fbf41af6c0a88e3d73b6cc07088874060
mtayllan <messiastayllan@gmail.com>
2024-01-08 21:06:46 -0300

    Add stimulus-use

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,8 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "date-fns", to: "https://ga.jspm.io/npm:date-fns@3.0.6/index.mjs"
+pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.52.2/dist/index.js"
