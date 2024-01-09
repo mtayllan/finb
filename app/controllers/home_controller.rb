b5f1d906d996882fb1c362c7ac92e254bdb4c8d1
mtayllan <messiastayllan@gmail.com>
2024-01-09 20:26:08 -0300

    Add monthly income and expenses summary to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @total_income_this_month = Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month).where('value > 0').sum(:value)
+    @total_expenses_this_month = Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month).where('value < 0').sum(:value)
   end
 end
