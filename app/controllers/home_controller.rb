453f86ad09c54a55110b07a9f15828955fa92f53
mtayllan <messiastayllan@gmail.com>
2024-01-14 08:28:06 -0300

    Add report of transactions by category and from last 3 months on main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @total_income_this_month = Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month).where('value > 0').sum(:value)
-    @total_expenses_this_month = Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month).where('value < 0').sum(:value)
+    current_month = Date.today.beginning_of_month..Date.today.end_of_month
+
+    @total_income_this_month = Transaction.where(date: current_month).where('value > 0').sum(:value)
+    @total_expenses_this_month = Transaction.where(date: current_month).where('value < 0').sum(:value)
 
     @expenses_by_category_this_month =
-      Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month)
+      Transaction.where(date: current_month)
                  .where('value < 0')
                  .group(:category)
                  .sum(:value)
@@ -12,7 +14,28 @@ class HomeController < ApplicationController
                  .to_h
 
     @income_by_category_this_month =
-      Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month)
+      Transaction.where(date: current_month)
+                .where('value > 0')
+                .group(:category)
+                .sum(:value)
+                .sort_by { -_1[1] }
+                .to_h
+
+    last_tree_months = (Date.today - 3.months).beginning_of_month..(Date.today - 1.month).end_of_month
+
+    @total_income_last_three = Transaction.where(date: last_tree_months).where('value > 0').sum(:value)
+    @total_expenses_last_three = Transaction.where(date: last_tree_months).where('value < 0').sum(:value)
+
+    @expenses_by_category_last_three =
+      Transaction.where(date: last_tree_months)
+                  .where('value < 0')
+                  .group(:category)
+                  .sum(:value)
+                  .sort_by { _1[1] }
+                  .to_h
+
+    @income_by_category_last_three =
+      Transaction.where(date: last_tree_months)
                 .where('value > 0')
                 .group(:category)
                 .sum(:value)
