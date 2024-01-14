9e241d4353d4c55817e84595d77111b8ac24e3c7
mtayllan <messiastayllan@gmail.com>
2024-01-14 08:16:20 -0300

    Add report of transactions by category on main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,21 @@ class HomeController < ApplicationController
   def index
     @total_income_this_month = Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month).where('value > 0').sum(:value)
     @total_expenses_this_month = Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month).where('value < 0').sum(:value)
+
+    @expenses_by_category_this_month =
+      Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month)
+                 .where('value < 0')
+                 .group(:category)
+                 .sum(:value)
+                 .sort_by { _1[1] }
+                 .to_h
+
+    @income_by_category_this_month =
+      Transaction.where(date: Date.today.beginning_of_month..Date.today.end_of_month)
+                .where('value > 0')
+                .group(:category)
+                .sum(:value)
+                .sort_by { -_1[1] }
+                .to_h
   end
 end
