0d1f789df70d4ffd00b148b591199d342e964563
mtayllan <messiastayllan@gmail.com>
2024-01-22 19:47:30 -0300

    Remove subcategories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,4 @@
 class Category < ApplicationRecord
-  belongs_to :parent_category, optional: true
-
-  has_many :sub_categories, class_name: "Category", foreign_key: "parent_category_id", dependent: :destroy
-
   validates :name, :color, :icon, presence: true
 
   def icon_svg
