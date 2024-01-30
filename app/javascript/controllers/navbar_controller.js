e4323f41388c803a67e5ee9d5fbeec09e2a4fa3e
mtayllan <messiastayllan@gmail.com>
2024-01-30 20:59:49 -0300

    Improve style of navbar and move it to a component

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,9 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
+import { useClickOutside } from 'stimulus-use'
 
 export default class extends Controller {
   static targets = ["mobile"]
 
+  connect() {
+    useClickOutside(this);
+  }
+
+  clickOutside() {
+    this.mobileTarget.classList.add("hidden")
+  }
+
   toggle() {
-    this.mobileTarget.classList.toggle("hidden")
+    this.mobileTarget.classList.toggle("hidden");
   }
 }
