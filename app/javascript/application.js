6560b9e233d2a1d6fccb36d9164cc38d101ae24c
mtayllan <messiastayllan@gmail.com>
2024-02-07 21:24:01 -0300

    Add category charts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "echarts"
+
+echarts.registerTheme('finb', {
+  backgroundColor: 'hsl(var(--background))',
+  label: {
+    color: 'hsl(var(--foreground))'
+  },
+  legend: {
+    textStyle: {
+      fontFamily: 'Inter var',
+      color: 'hsl(var(--foreground))'
+    }
+  },
+  title: {
+    textStyle: {
+      fontFamily: 'Inter var',
+      color: 'hsl(var(--primary))'
+    }
+  }
+})
