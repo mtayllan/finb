6a5e3bd390ecc397e613b91665d2a5a13b4165a7
mtayllan <messiastayllan@gmail.com>
2024-02-12 10:53:36 -0300

    Fix rubocop config

--- test/helpers/icon_helper_test.rb
+++ test/helpers/icon_helper_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class IconHelperTest < ActionView::TestCase
   test "render an svg with the icon" do
-    assert_equal render_icon('student', size: 10, background: "#ffffff", fill: "black"),
+    assert_equal render_icon("student", size: 10, background: "#ffffff", fill: "black"),
     <<~HTML
       <svg
         xmlns="http://www.w3.org/2000/svg"
