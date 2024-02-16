9631019604f254e0c59974ee92e5914ef48c20ca
mtayllan <messiastayllan@gmail.com>
2024-02-15 22:12:32 -0300

    Add show account page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,22 @@
 class AccountsController < ApplicationController
-  before_action :set_account, only: %i[ edit update destroy ]
+  before_action :set_account, only: %i[ show edit update destroy ]
 
   def index
     @accounts = Account.all.order(:name)
   end
 
   def show
+    @month = params[:month] ? Date.parse(params[:month]) : Date.current
+    filter = { date: @month.beginning_of_month..@month.end_of_month }
+    filter[:category_id] = params[:category_id] if params[:category_id]
+    transactions = Transaction.includes(:category).where(filter).order(date: :desc, created_at: :desc)
+    transfers = [] if filter[:category_id]
+    transfers ||= Transfer.includes(:origin_account, :target_account)
+                        .where(date: filter[:date])
+                        .where("(origin_account_id = :account_id OR target_account_id = :account_id)", account_id: @account.id)
+                        .order(date: :desc, created_at: :desc)
+
+    @transactions_and_transfers = (transactions + transfers).sort_by(&:date).reverse
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :transfers, except: :show
   resources :transactions, except: :show
-  resources :accounts, except: :show
+  resources :accounts
   resources :categories, except: :show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
