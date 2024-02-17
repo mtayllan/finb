deaa64ada7e6b2e463a993d89bde929804e22228
mtayllan <messiastayllan@gmail.com>
2024-02-17 15:15:22 -0300

    Redirect to account url after create, update or delete transaction

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -17,27 +17,29 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create expense transaction" do
+    account = create(:account)
     assert_difference("Transaction.count") do
       post transactions_url, params: { transaction: {
         date: Date.current, value: 10, description: "test", transaction_type: "expense",
-        account_id: create(:account).id, category_id: create(:category).id
+        account_id: account.id, category_id: create(:category).id
       } }
     end
 
-    assert_redirected_to transactions_url
+    assert_redirected_to account_url(account)
     assert_equal flash[:notice], "Transaction was successfully created."
     assert_equal Transaction.last.value, -10
   end
 
   test "should create income transaction" do
+    account = create(:account)
     assert_difference("Transaction.count") do
       post transactions_url, params: { transaction: {
         date: Date.current, value: 10, description: "test", transaction_type: "income",
-        account_id: create(:account).id, category_id: create(:category).id
+        account_id: account.id, category_id: create(:category).id
       } }
     end
 
-    assert_redirected_to transactions_url
+    assert_redirected_to account_url(account)
     assert_equal flash[:notice], "Transaction was successfully created."
     assert_equal Transaction.last.value, 10
   end
@@ -59,7 +61,7 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
     @transaction = create(:transaction)
 
     patch transaction_url(@transaction), params: { transaction: { name: Faker::Bank.name } }
-    assert_redirected_to transactions_url
+    assert_redirected_to account_url(@transaction.account)
     assert_equal flash[:notice], "Transaction was successfully updated."
   end
 
@@ -77,7 +79,7 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
       delete transaction_url(@transaction)
     end
 
-    assert_redirected_to transactions_url
+    assert_redirected_to account_url(@transaction.account)
     assert_equal flash[:notice], "Transaction was successfully destroyed."
   end
 end
