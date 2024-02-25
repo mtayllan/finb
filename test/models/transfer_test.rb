854e658825c8ffc565656b6e73561e3b3a1a0008
mtayllan <messiastayllan@gmail.com>
2024-02-25 18:24:59 -0300

    Fix tests

--- test/models/transfer_test.rb
+++ test/models/transfer_test.rb
@@ -2,15 +2,15 @@ require "test_helper"
 
 class TransferTest < ActiveSupport::TestCase
   test "invalid with equal origin and target accounts" do
-    account = Account.create(name: "Test")
+    account = create(:account)
     transfer = Transfer.new(value: 50, date: Date.today, origin_account: account, target_account: account)
     assert_not transfer.valid?
     assert_includes transfer.errors[:target_account], "must be different from Origin Account"
   end
 
   test "valid with different origin and target accounts" do
-    origin_account = Account.create(name: "Origin Account")
-    target_account = Account.create(name: "Target Account")
+    origin_account = create(:account)
+    target_account = create(:account)
     transfer = Transfer.new(value: 50, date: Date.today, origin_account:, target_account:)
     assert transfer.valid?
   end
