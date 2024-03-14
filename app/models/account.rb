fb79a147494f923f6d683057f8088c222bd79a04
mtayllan <messiastayllan@gmail.com>
2024-03-14 18:17:26 -0300

    Add delete account function

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,8 @@
 class Account < ApplicationRecord
   has_many :transactions, dependent: :destroy
   has_many :balances, dependent: :destroy
+  has_many :transfers_as_origin, class_name: "Transfer", foreign_key: "origin_account_id", dependent: :destroy
+  has_many :transfers_as_target, class_name: "Transfer", foreign_key: "target_account_id", dependent: :destroy
 
   validates :name, :color, :initial_balance, :kind, :initial_balance_date, presence: true
 
@@ -9,13 +11,5 @@ class Account < ApplicationRecord
   def update_balance
     update(balance: initial_balance + transactions.sum(:value) - transfers_as_origin.sum(:value) + transfers_as_target.sum(:value))
     Account::UpdateBalances.call(self)
-  end
-
-  def transfers_as_origin
-    Transfer.where(origin_account: self)
-  end
-
-  def transfers_as_target
-    Transfer.where(target_account: self)
   end
 end
