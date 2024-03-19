f047404722ea72f7091c1ace855efef76180c4a2
mtayllan <messiastayllan@gmail.com>
2024-03-19 15:35:10 -0300

    Add more tests to accounts controller

--- test/controllers/accounts_controller_test.rb
+++ test/controllers/accounts_controller_test.rb
@@ -58,4 +58,59 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to accounts_url
     assert_equal flash[:notice], "Account was successfully destroyed."
   end
+
+  test "show checking account report" do
+    account = create(:account, kind: :checking)
+
+    get account_url(account)
+
+    assert_select "turbo-frame#expenses_by_categories"
+    assert_select "turbo-frame#income_by_categories"
+  end
+
+  test "show savings account report" do
+    account = create(:account, kind: :savings)
+
+    get account_url(account)
+
+    assert_select "turbo-frame#daily_balances"
+  end
+
+  test "show investment account report" do
+    account = create(:account, kind: :investment)
+
+    get account_url(account)
+
+    assert_select "turbo-frame#daily_balances"
+  end
+
+  test "show credit_card account report" do
+    account = create(:account, kind: :credit_card)
+
+    get account_url(account)
+
+    assert_select "turbo-frame#expenses_by_categories"
+  end
+
+  test "show account transactions filtered by category" do
+    account = create(:account)
+    category = create(:category)
+    other_category = create(:category)
+    create_list(:transaction, 3, account: account, category:)
+    create_list(:transaction, 2, account: account, category: other_category)
+
+    get account_url(account, params: { category_id: category.id })
+
+    assert_select "tr", 4
+  end
+
+  test "show account transactions filtered by month" do
+    account = create(:account, initial_balance_date: 3.months.ago)
+    create_list(:transaction, 3, account: account, date: 1.month.ago)
+    create_list(:transaction, 2, account: account, date: 2.months.ago)
+
+    get account_url(account, params: { month: 1.month.ago.iso8601 })
+
+    assert_select "tr", 4
+  end
 end
