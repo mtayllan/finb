dfb3b7873a0c55bb938f7622cd6a421f7865dddd
mtayllan <messiastayllan@gmail.com>
2024-03-20 23:00:17 -0300

    Fix coverage report

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
-require "simplecov"
-SimpleCov.start("rails") do
-  enable_coverage :branch
+
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.start("rails") do
+    enable_coverage :branch
+  end
 end
 
 ENV["RAILS_ENV"] ||= "test"
@@ -11,6 +14,16 @@ module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
+
+    if ENV["COVERAGE"]
+      parallelize_setup do |worker|
+        SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+      end
+
+      parallelize_teardown do |worker|
+        SimpleCov.result
+      end
+    end
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
