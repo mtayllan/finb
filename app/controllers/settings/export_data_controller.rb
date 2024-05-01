e87160e64f31356b4adbcbb4f54974d06ea99b70
mtayllan <messiastayllan@gmail.com>
2024-05-01 08:30:37 -0300

    Rename DataManagement Controller to Settings

--- app/controllers/settings/export_data_controller.rb
+++ app/controllers/settings/export_data_controller.rb
@@ -1,4 +1,4 @@
-class DataManagement::ExportsController < ApplicationController
+class Settings::ExportDataController < ApplicationController
   def create
     send_data(DataManagement::Export.new.to_json, filename: "finb.json", type: "application/json")
   end
