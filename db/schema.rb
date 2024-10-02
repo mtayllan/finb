dd938b1845dbe8c4b71605b00ada191161f9b0da
mtayllan <messiastayllan@gmail.com>
2024-10-01 22:17:15 -0300

    Upgrade to rails 7.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_04_003539) do
+ActiveRecord::Schema[7.2].define(version: 2024_06_04_003539) do
   create_table "account_balances", force: :cascade do |t|
     t.integer "account_id", null: false
     t.date "date", null: false
