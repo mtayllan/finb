b76eca7dde1b7118ef479816839ad0e3e272bd99
mtayllan <messiastayllan@gmail.com>
2025-01-27 21:10:04 -0300

    Permit calculation of balance on future dates

--- app/models/account/update_balances.rb
+++ app/models/account/update_balances.rb
@@ -2,13 +2,15 @@ module Account::UpdateBalances
   def self.call(account)
     start_date = account.initial_balance_date
 
-    transactions = account.transactions.where(date: start_date..)
-    transfers_as_origin = account.transfers_as_origin.where(date: start_date..)
-    transfers_as_target = account.transfers_as_target.where(date: start_date..)
+    transactions = account.transactions.where(date: start_date..).order(:date)
+    transfers_as_origin = account.transfers_as_origin.where(date: start_date..).order(:date)
+    transfers_as_target = account.transfers_as_target.where(date: start_date..).order(:date)
 
-    previous_balance = account.balances.find_by(date: start_date - 1.day)&.balance || account.initial_balance
+    final_date = [transactions.last&.date, transfers_as_origin.last&.date, transfers_as_target.last&.date, Date.current].compact.max
+
+    previous_balance = account.initial_balance
     data = []
-    start_date.upto(Date.current) do |date|
+    start_date.upto(final_date) do |date|
       balance =
         previous_balance +
         transactions.select { |it| it.date == date }.sum(&:value) +
