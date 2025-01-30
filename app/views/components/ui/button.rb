943cd9408e14b3d3ff11b48ab94e81ea3a39e76a
mtayllan <messiastayllan@gmail.com>
2025-01-30 13:53:00 -0300

    Add simple button component

---
+++ app/views/components/ui/button.rb
@@ -0,0 +1,14 @@
+class UI::Button < ViewComponent::Base
+  def initialize(href: nil, data: {}, form: nil)
+    @href = href
+    @data = data
+    @form = form
+  end
+
+  def classes
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md
+     text-sm font-medium transition-colors focus-visible:outline-none
+     focus-visible:ring-1 focus-visible:ring-ring bg-primary text-primary-foreground
+     shadow hover:bg-primary/90 px-4 py-2"
+  end
+end
