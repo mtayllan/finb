9465393785a88b726c4b6bcddee7844e13ff5ab5
mtayllan <messiastayllan@gmail.com>
2025-03-09 11:05:49 -0300

    Add balance fixing functionality with new controller and views

---
+++ app/controllers/accounts/balance_fixes_controller.rb
@@ -0,0 +1,35 @@
+class Accounts::BalanceFixesController < ApplicationController
+  before_action :set_account
+
+  def new
+    @transaction = Transaction.new
+    @categories = Current.user.categories.order(:name)
+  end
+
+  def create
+    correct_balance = BigDecimal(params[:balance_fix][:correct_balance])
+    diff = correct_balance - @account.balance
+
+    @transaction = Transaction.new(
+      account: @account,
+      description: params[:transaction][:description],
+      category_id: params[:transaction][:category_id],
+      value: diff,
+      date: Date.current
+    )
+
+    if @transaction.save
+      @account.update_balance
+      redirect_to account_path(@account), notice: "Account balance was successfully fixed."
+    else
+      @categories = Current.user.categories.order(:name)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_account
+    @account = Current.user.accounts.find(params[:account_id])
+  end
+end
