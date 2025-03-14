283dbcdc19aeaf07c75035909fbe006c4889cdf0
mtayllan <messiastayllan@gmail.com>
2025-03-14 07:49:55 -0300

    Update styles to use Daisy UI

--- app/views/components/ui/button.rb
+++ app/views/components/ui/button.rb
@@ -6,9 +6,6 @@ class UI::Button < ViewComponent::Base
   end
 
   def classes
-    "inline-flex items-center justify-center whitespace-nowrap rounded-md
-     text-sm font-medium transition-colors focus-visible:outline-hidden
-     focus-visible:ring-1 focus-visible:ring-ring bg-primary text-primary-foreground
-     shadow hover:bg-primary/90 px-4 py-2"
+    "btn btn-primary"
   end
 end
