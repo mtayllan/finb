1afa541a0e3f49857f6441e067227a0ad9ff5681
mtayllan <messiastayllan@gmail.com>
2025-05-26 20:22:22 -0300

    fix standardrb warnings

--- test/models/transfer_test.rb
+++ test/models/transfer_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class TransferTest < ActiveSupport::TestCase
   test "invalid with equal origin and target accounts" do
     account = accounts(:bank_one)
-    transfer = Transfer.new(value: 50, date: Date.today, origin_account: account, target_account: account)
+    transfer = Transfer.new(value: 50, date: Time.zone.today, origin_account: account, target_account: account)
     assert_not transfer.valid?
     assert_includes transfer.errors[:target_account], "must be different from Origin Account"
   end
@@ -11,7 +11,7 @@ class TransferTest < ActiveSupport::TestCase
   test "valid with different origin and target accounts" do
     origin_account = accounts(:bank_one)
     target_account = accounts(:savings_one)
-    transfer = Transfer.new(value: 50, date: Date.today, origin_account:, target_account:)
+    transfer = Transfer.new(value: 50, date: Time.zone.today, origin_account:, target_account:)
     assert transfer.valid?
   end
 end
