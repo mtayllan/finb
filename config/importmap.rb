b6599fdd6f9c0293fab8431af2826cf79e907db4
mtayllan <messiastayllan@gmail.com>
2025-05-26 21:34:51 -0300

    rollback date-fns to mjs

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "date-fns" # @4.1.0
+pin "date-fns", to: "https://ga.jspm.io/npm:date-fns@3.6.0/index.mjs"
 pin "stimulus-use" # @0.52.3
 pin "echarts", to: "echarts.min.js"
 pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.12
