5d35cecf01e57d0184bdd2fb80185c6158c3f87f
mtayllan <messiastayllan@gmail.com>
2025-06-17 22:11:37 -0300

    change credit_card_expiration_day column type from date to integer in accounts table

--- db/migrate/20250617011906_add_dates_to_accounts.rb
+++ db/migrate/20250617011906_add_dates_to_accounts.rb
@@ -1,5 +1,5 @@
 class AddDatesToAccounts < ActiveRecord::Migration[8.0]
   def change
-    add_column :accounts, :credit_card_expiration_day, :date
+    add_column :accounts, :credit_card_expiration_day, :integer
   end
 end
