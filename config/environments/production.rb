6a86445d1dcbebc944eb95ed44cea51591c9e8f3
mtayllan <messiastayllan@gmail.com>
2025-11-09 07:51:54 -0300

    Upgrade to rails 8.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = ENV.fetch("USE_SSL", "true") == "true"
+  # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = ENV.fetch("USE_SSL", "true") == "true"
@@ -37,7 +38,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
   config.logger = ActiveSupport::TaggedLogging.logger(STDOUT) # standard:disable Style/GlobalStdStream
 
-  # Change to "debug" to log everything (including potentially personally-identifiable information!)
+  # Change to "debug" to log everything (including potentially personally-identifiable information!).
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Prevent health checks from clogging up the logs.
@@ -59,7 +60,7 @@ Rails.application.configure do
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = {host: "example.com"}
 
-  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
+  # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
   #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
   #   password: Rails.application.credentials.dig(:smtp, :password),
--- db/schema.rb
+++ db/schema.rb
@@ -10,66 +10,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_26_183529) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_09_103730) do
   create_table "account_balances", force: :cascade do |t|
     t.integer "account_id", null: false
-    t.date "date", null: false
     t.decimal "balance", precision: 19, scale: 2, null: false
     t.datetime "created_at", null: false
+    t.date "date", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id", "date"], name: "index_account_balances_on_account_id_and_date", unique: true
     t.index ["account_id"], name: "index_account_balances_on_account_id"
   end
 
   create_table "accounts", force: :cascade do |t|
-    t.string "name", null: false
-    t.decimal "initial_balance", precision: 9, scale: 2, default: "0.0", null: false
     t.decimal "balance", precision: 9, scale: 2, default: "0.0", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "color", default: "#000000", null: false
-    t.integer "kind", default: 0, null: false
-    t.date "initial_balance_date", null: false
-    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
     t.integer "credit_card_expiration_day"
+    t.decimal "initial_balance", precision: 9, scale: 2, default: "0.0", null: false
+    t.date "initial_balance_date", null: false
+    t.integer "kind", default: 0, null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "color", default: "#000000", null: false
+    t.datetime "created_at", null: false
     t.string "icon", default: "", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "credit_card_statements", force: :cascade do |t|
-    t.date "paid_at"
     t.integer "account_id", null: false
-    t.date "month", null: false
-    t.decimal "value", default: "0.0"
     t.datetime "created_at", null: false
+    t.date "month", null: false
+    t.date "paid_at"
     t.datetime "updated_at", null: false
+    t.decimal "value", default: "0.0"
     t.index ["account_id"], name: "index_credit_card_statements_on_account_id"
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.string "token", null: false
     t.datetime "created_at", null: false
+    t.string "token", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "splits", force: :cascade do |t|
-    t.integer "payer_transaction_id", null: false
-    t.integer "borrower_transaction_id"
-    t.integer "borrower_id", null: false
     t.decimal "amount_borrowed", null: false
+    t.integer "borrower_id", null: false
+    t.integer "borrower_transaction_id"
     t.date "confirmed_at"
     t.datetime "created_at", null: false
+    t.integer "payer_transaction_id", null: false
     t.datetime "updated_at", null: false
     t.index ["borrower_id"], name: "index_splits_on_borrower_id"
     t.index ["borrower_transaction_id"], name: "index_splits_on_borrower_transaction_id", unique: true, where: "borrower_transaction_id IS NOT NULL"
@@ -78,64 +78,64 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_26_183529) do
 
   create_table "statement_analyses", force: :cascade do |t|
     t.integer "account_id", null: false
+    t.datetime "created_at", null: false
+    t.integer "credit_card_statement_id"
     t.integer "status", default: 0, null: false
     t.integer "total_rows", null: false
-    t.integer "credit_card_statement_id"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_statement_analyses_on_account_id"
     t.index ["credit_card_statement_id"], name: "index_statement_analyses_on_credit_card_statement_id"
   end
 
   create_table "statement_analysis_items", force: :cascade do |t|
-    t.integer "statement_analysis_id", null: false
-    t.string "description", null: false
-    t.date "date", null: false
-    t.decimal "value", precision: 9, scale: 2, null: false
     t.integer "category_id"
-    t.boolean "should_import", default: true, null: false
-    t.integer "row_number", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.date "date", null: false
+    t.string "description", null: false
     t.string "original_description"
+    t.integer "row_number", null: false
+    t.boolean "should_import", default: true, null: false
+    t.integer "statement_analysis_id", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "value", precision: 9, scale: 2, null: false
     t.index ["category_id"], name: "index_statement_analysis_items_on_category_id"
     t.index ["statement_analysis_id"], name: "index_statement_analysis_items_on_statement_analysis_id"
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.string "description"
-    t.decimal "value", precision: 9, scale: 2, default: "0.0", null: false
-    t.integer "category_id", null: false
     t.integer "account_id"
-    t.date "date", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "credit_card_statement_id"
+    t.date "date", null: false
+    t.string "description"
+    t.boolean "exclude_from_reports", default: false, null: false
     t.boolean "from_split", default: false
     t.decimal "report_value", precision: 9, scale: 2
-    t.boolean "exclude_from_reports", default: false, null: false
+    t.datetime "updated_at", null: false
+    t.decimal "value", precision: 9, scale: 2, default: "0.0", null: false
     t.index ["account_id"], name: "index_transactions_on_account_id"
     t.index ["category_id"], name: "index_transactions_on_category_id"
     t.index ["credit_card_statement_id"], name: "index_transactions_on_credit_card_statement_id"
   end
 
   create_table "transfers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.date "date", null: false
+    t.string "description"
     t.integer "origin_account_id", null: false
     t.integer "target_account_id", null: false
-    t.string "description"
-    t.decimal "value", precision: 10, scale: 2, null: false
-    t.date "date", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "value", precision: 10, scale: 2, null: false
     t.index ["origin_account_id"], name: "index_transfers_on_origin_account_id"
     t.index ["target_account_id"], name: "index_transfers_on_target_account_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
+    t.string "default_currency", default: "USD", null: false
     t.string "password_digest", null: false
     t.boolean "superuser", default: false
-    t.string "default_currency", default: "USD", null: false
+    t.string "username", null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
