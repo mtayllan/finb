bfafd441b807ec3d0243e08096076dd5947135e9
mtayllan <messiastayllan@gmail.com>
2026-05-16 14:51:18 -0300

    remove duplicated credit card routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "credit_cards/index"
-  get "credit_cards/show"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", :as => :rails_health_check
